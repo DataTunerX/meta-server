d7a0e876671458dbe8f8476cde0163bc4389ab3e
tigerK <yanru.lv@daocloud.io>
2023-11-20 10:20:58 +0800

    Fix(api): Modify some api definitions
    1. set finetunejob.spec.finetune.name *string

--- api/finetune/v1beta1/finetunejob_types.go
+++ api/finetune/v1beta1/finetunejob_types.go
@@ -87,7 +87,7 @@ type ScoringConfig struct {
 
 type FineTune struct {
 	// +kubebuilder:validation:Optional
-	Name *string `json:"name"`
+	Name *string `json:"name,omitempty"`
 	// +kubebuilder:validation:Required
 	FinetuneSpec FinetuneSpec `json:"finetuneSpec"`
 }
