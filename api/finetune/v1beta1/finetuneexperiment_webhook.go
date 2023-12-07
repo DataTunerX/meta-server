62fc7036e7946f666dbfdbd72b90172ced7f8ad2
tigerK <yanru.lv@daocloud.io>
2023-12-07 14:11:40 +0800

    feat(update controller-runtime verison):

--- api/finetune/v1beta1/finetuneexperiment_webhook.go
+++ api/finetune/v1beta1/finetuneexperiment_webhook.go
@@ -21,6 +21,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
 // log is for logging in this package.
@@ -49,25 +50,25 @@ func (r *FinetuneExperiment) Default() {
 var _ webhook.Validator = &FinetuneExperiment{}
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
-func (r *FinetuneExperiment) ValidateCreate() error {
+func (r *FinetuneExperiment) ValidateCreate() (warnings admission.Warnings, err error) {
 	finetuneexperimentlog.Info("validate create", "name", r.Name)
 
 	// TODO(user): fill in your validation logic upon object creation.
-	return nil
+	return nil, nil
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
-func (r *FinetuneExperiment) ValidateUpdate(old runtime.Object) error {
+func (r *FinetuneExperiment) ValidateUpdate(old runtime.Object) (warnings admission.Warnings, err error) {
 	finetuneexperimentlog.Info("validate update", "name", r.Name)
 
 	// TODO(user): fill in your validation logic upon object update.
-	return nil
+	return nil, nil
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
-func (r *FinetuneExperiment) ValidateDelete() error {
+func (r *FinetuneExperiment) ValidateDelete() (warnings admission.Warnings, err error) {
 	finetuneexperimentlog.Info("validate delete", "name", r.Name)
 
 	// TODO(user): fill in your validation logic upon object deletion.
-	return nil
+	return nil, nil
 }
